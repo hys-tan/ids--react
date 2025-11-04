2f64a6988196b175bfe41852cd2e7c75a0375e47
ELIOT <ramirezeliot3-1@hotmail.com>
2025-11-04 05:48:28 -0500

    v 1.2

--- src/components/layout/navbar.tsx
+++ src/components/layout/navbar.tsx
@@ -1,25 +1,37 @@
+import React from 'react';
 import styles from './navbar.module.css';
 import { getImageUrl } from '../../utils/image-helper';
 
-const Navbar = () => {
+const Navbar: React.FC = () => {
   return (
     <nav className={styles.navbar}>
-      <div className={styles.logo}>
-        <a href="/">
-          <img src={getImageUrl('logo.png')} alt="Idelsi" className={styles.logoImg} />
-        </a>
+      {/* Top section */}
+      <div className={styles.topNav}>
+        <span>Lunes - Sábado | 08:00 - 19:00</span>
       </div>
 
-      <div className={styles.navActions}>
-        <ul className={styles.navLinks}>
-          <li><a href="/#inicio">Inicio</a></li>
-          <li><a href="/#servicios">Servicios</a></li>
-          <li><a href="/#nosotros">Nosotros</a></li>
-          <li><a href="/#contacto">Contáctanos</a></li>
-        </ul>
-        <button className={styles.ctaButton}>
-          Cotiza ahora
-        </button>
+      {/* Divider */}
+      <hr className={styles.divider} />
+
+      {/* Bottom section */}
+      <div className={styles.bottomNav}>
+        {/* Logo */}
+        <a href="/">
+          <img src={getImageUrl('logo.png')} alt="Idelsi" className={styles.logo} />
+        </a>
+
+        <div className={styles.navActions}>
+          {/* Navigation links */}
+          <ul className={styles.navLinks}>
+            <li><a href="/#inicio">Inicio</a></li>
+            <li><a href="/#servicios">Servicios</a></li>
+            <li><a href="/#nosotros">Nosotros</a></li>
+            <li><a href="/#contactanos">Contáctanos</a></li>
+          </ul>
+          <button className={styles.ctaButton}>
+            Cotiza ahora
+          </button>
+        </div>
       </div>
     </nav>
   );
