6ecda0ac93c736d11dddea15a1079f212f227ea8
ELIOT <ramirezeliot3-1@hotmail.com>
2025-11-16 20:46:43 -0500

    v 1.10 homePage 100

--- src/sections/homePage.tsx
+++ src/sections/homePage.tsx
@@ -25,6 +25,11 @@ const HomePage: React.FC = () => {
         <h1 className={styles.title}>Servicio Técnico Especializado</h1>
         <p className={styles.description}>Ofrecemos mantenimiento preventivo y correctivo, grupos electrógenos, fabricación de tableros de transferencia y mantenimiento de motores eléctricos.</p>
       </div>
+      <div className={styles.shapeDivider}>
+          <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
+              <path d="M649.97 0L599.91 54.12 550.03 0 0 0 0 120 1200 120 1200 0 649.97 0z" className={styles.shapeFill}></path>
+          </svg>
+      </div>
     </div>
     // El segundo contenedor irá aquí abajo, pero lo haremos después.
   );
