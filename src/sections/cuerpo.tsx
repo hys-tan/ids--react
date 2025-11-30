a0ba1f7446d83853c27d0bc7a996016cc6089b69
ELIOT <ramirezeliot3-1@hotmail.com>
2025-11-30 01:59:08 -0500

    v 1.16 sp2 deleted

--- src/sections/cuerpo.tsx
+++ src/sections/cuerpo.tsx
@@ -68,13 +68,7 @@ const Cuerpo: React.FC = () => {
           </p>
         </div>
       </div>
-      
-      {/* --- SHAPE DIVIDER --- */}
-      <div className={styles.shapeDivider}>
-        <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
-          <path d="M598.97 114.72L0 0 0 120 1200 120 1200 0 598.97 114.72z" className={styles.shapeFill}></path>
-        </svg>
-      </div>
+
     </section>
   );
 };
