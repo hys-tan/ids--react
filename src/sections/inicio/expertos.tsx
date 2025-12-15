538608b23e4cb978007dcb2891fec4faadf0762b
ELIOT <ramirezeliot3-1@hotmail.com>
2025-12-15 03:56:52 -0500

    v 2.46.3

--- src/sections/inicio/expertos.tsx
+++ src/sections/inicio/expertos.tsx
@@ -6,7 +6,6 @@ const Expertos: React.FC = () => {
         <div className={styles.expertosContainer}>
             <div className={styles.contentWrapper}>
                 <h2 className={styles.title}>¡Juntos, Impulsamos el Éxito de tu Proyecto!</h2>
-                {/*<button className={styles.contactButton}>Hablar con un experto</button>*/}
             </div>
         </div>
     );
